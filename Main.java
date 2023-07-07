05c7426b3f3b485f275b49347e9ca576714c73e9
iBreadd <124068827+iBreadd@users.noreply.github.com>
2023-07-07 15:54:12 +0300

    Multiplication functions added.

--- Main.java
+++ Main.java
@@ -63,6 +63,51 @@ public class Main {
             System.out.println("Матриците не могат да бъдат събрани. Размерностите им не съвпадат.");
         }
     }
+    public static int[][] multiplyMatrices(int[][] matrix1, int[][] matrix2) {
+        int rows1 = matrix1.length;
+        int columns1 = matrix1[0].length;
+        int rows2 = matrix2.length;
+        int columns2 = matrix2[0].length;
+
+        if (columns1 != rows2) {
+            return null;
+        }
+
+        int[][] result = new int[rows1][columns2];
+
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < columns2; j++) {
+                for (int k = 0; k < columns1; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean validateMatrixDimensionsForMultiplication(int[][] matrix1, int[][] matrix2) {
+        int columns1 = matrix1[0].length;
+        int rows2 = matrix2.length;
+
+        return columns1 == rows2;
+    }
+
+    public static void performMatrixMultiplication(int[][] matrix1, int[][] matrix2) {
+        if (validateMatrixDimensionsForMultiplication(matrix1, matrix2)) {
+            int[][] product = multiplyMatrices(matrix1, matrix2);
+
+            if (product != null) {
+                System.out.println("Резултатът от умножението на двете матрици е:");
+                printMatrix(product);
+            } else {
+                System.out.println("Матриците не могат да бъдат умножени. Невалидни размерности.");
+            }
+        } else {
+            System.out.println("Матриците не могат да бъдат умножени. Невалидни размерности.");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -87,5 +132,7 @@ public class Main {
         printMatrix(matrix2);
 
         performMatrixAddition(matrix1, matrix2);
+
+        performMatrixMultiplication(matrix1, matrix2);
     }
 }
